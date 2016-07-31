60f9e13d6291e9fcd3429c982d3a701ea02458de
notfood <notfood@users.noreply.github.com>
2016-07-31 11:55:52 -0500

    Initial commit

---
+++ Source/MiniaturisationDef.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Verse;
+
+namespace Miniaturisation
+{
+	public class MiniaturisationDef : Def
+	{
+		#region XML Data
+		public string requiredMod;
+		public List<string> targetsDefNames = new List<string>();
+		#endregion
+	}
+}
+
